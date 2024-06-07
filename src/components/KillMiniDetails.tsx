773d471a5fc399d4c5aa9344504ef4b598fdc207
notes <notes@sigil.systems>
2024-06-07 02:12:56 -0500

    Added a lot of shit time to make it work

---
+++ src/components/KillMiniDetails.tsx
@@ -0,0 +1,8 @@
+
+export default function KillMiniDetails() {
+    return (
+        <div className="KillMiniDetails">
+            DETAILS GO HERE
+        </div>
+    );
+}
---
+++ src/pages/Snacker.tsx
@@ -0,0 +1,15 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+
+export default function Snacker() {
+    return (
+        <div className="Snacker">
+            <div className="Header">
+                <ConnectButton />
+            </div>
+
+            Snax
+        </div>
+
+    );
+}
